87eda57ff9cfd36def7153a80073bf8a91cdf3d6
akankshaverm <priyaakanksha0507@gmail.com>
2025-02-19 02:01:17 +0530

    changes in route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Livewire\Admin\Category\ManageCategory;
-use App\Livewire\Admin\Coupon\CreateCoupon ;
+use App\Livewire\Admin\Coupon\CreateCoupon;
 use App\Livewire\Admin\Coupon\ManageCoupon;
 use App\Livewire\Admin\Product\CreateProduct;
 use App\Livewire\Admin\Product\ManageProduct;
@@ -12,9 +12,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home',Home::class)->name('home');
+Route::get('/home', Home::class)->name('home');
 
- Route::prefix('admin')->group(function(){
+//Admin Route
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.adminbase');
+    })->name('admin');
     Route::get('/manage-category', ManageCategory::class)->name('category.manage-category');
     Route::get('/create-product', CreateProduct::class)->name('product.create-product');
     Route::get('/manage-product', ManageProduct::class)->name('product.manage-product');
@@ -22,9 +26,5 @@ Route::get('/home',Home::class)->name('home');
     Route::get('/manage-coupon', ManageCoupon::class)->name('coupon.manage-coupon');
 });
 
-Route::get('/admin',function(){
-    return view('admin.adminbase');
-})->name('admin');
-Route::get("/register",Register::class)->name('register');
-
-
+//Authentication Route
+Route::get("/register", Register::class)->name('register');
